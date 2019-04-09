c6f9e529ca98212bafa86d2e6a49ac468e1c6562
jcurwen31 <jcurwen31@gmail.com>
2019-04-09 22:12:40 +0200

    creation des posts et affichage

--- mb_project/mb_project/settings.py
+++ mb_project/mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@%t@75y=uupz&(uo5v!=rwese#qh(f1y9*nouyawj4jgox87qv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mb_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
